4fd597631e40ca0334780281e1b0e44a544e3754
Yohan <yohan.battesti@gmail.com>
2022-07-18 14:29:00 +0100

    connection a la bdd avec sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const app = express();
+
+const Db = require("./db/db.js")
+
+Db.sync().then((console.log("connexion a la bdd")))
+.catch(error => console.log(error))
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
