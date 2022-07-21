b3bb779d51790c881ac05f2a85a61cf1b848d415
Yohan <yohan.battesti@gmail.com>
2022-07-21 11:28:29 +0100

    config de la bdd

---
+++ backend/config/db.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "username": "root",
+    "password": null,
+    "database": "projet7",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+
+// {
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "projet7",
+//     "host": "localhost",
+//     "dialect": "mysql"
+// }
