c568fb66b5b88b34d9a7b190b27357baa3f9df39
Yohan <yohan.battesti@gmail.com>
2022-07-21 11:29:58 +0100

    Création du models et de la route user

---
+++ backend/models/user.js
@@ -0,0 +1,48 @@
+const {Sequelize , DataTypes} = require('sequelize')
+const sequelize = require("../db/db.js");
+
+// Création du modèle User qui servira à alimenter la table Users dans notre BDD --------------------------------------
+// module.exports = (sequelize, Sequelize) => {
+const User = sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    name: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+
+    username: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, // Utilisation d'un regex pour le format d'adresse mail
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      default: false,
+    },
+  });
+
+//   return User;
+// };
+
+module.exports = User;
---
+++ backend/routes/users.js
@@ -0,0 +1,9 @@
+// Mise en place des routes concernant la gestion des users ---------------------------------------------------------------------
+const express = require('express'); 
+const router = express.Router(); 
+
+const userCtrl = require('../controllers/users') // Utilsiation du controlleur User
+
+router.post('/signup', userCtrl.signup); // Création d'un nouvel user avec contrôle du format de password
+
+module.exports = router; // Exportation du router
