04749aafcf3de8eb05c9414f1a81a2345bad00e2
Yohan <yohan.battesti@gmail.com>
2022-07-29 11:34:00 +0100

    Mise en place de la connection et verif du token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,10 @@
 const User = require("../models/user"); // Récupération des modèles Sequelize
 const bcrypt = require("bcrypt"); // Bcrypt permet de crypter le password et de le comparer
+const jwt = require('jsonwebtoken')
 
 // Gestion de la création d'un utilisateur et cryptage du mot de passe  ---------------------------------------------------------------
 exports.signup = (req, res, next) => {
-   console.log(req.body)
-  //  res.status(201).json({ message: "Je suis un guignole!" })
+  //console.log(req.body)
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -22,4 +22,32 @@ exports.signup = (req, res, next) => {
     })
 
     .catch((error) => res.status(500).json({ error , message: "Problème mdp"}));
+};
+
+/* Connexion par mot de passe */
+exports.login = (req, res, next) => {
+  User.findOne({  where: {
+      username: req.body.username,
+    }, })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+                { userId: user._id},
+                 process.env.SECRET_TOKEN,
+                { expiresIn: '24h'}
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userCtrl = require('../controllers/users') // Utilisation du controlleur User
 
 router.post('/signup', userCtrl.signup); // Création d'un nouvel user avec contrôle du format de password
+router.post('/login', userCtrl.login);
 
 module.exports = router; // Exportation du router
