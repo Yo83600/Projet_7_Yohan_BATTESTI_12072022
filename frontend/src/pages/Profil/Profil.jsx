2f45906a74aad365b534b842c8d62b4101011e3f
Yohan <yohan.battesti@gmail.com>
2022-08-30 12:17:40 +0100

    Correction css et token dans page profil

--- frontend/src/pages/Profil/Profil.jsx
+++ frontend/src/pages/Profil/Profil.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import EditProfil from "../../components/EditProfil/EditProfil";
 import './Profil.css'
 
 function Profil() {
+
+    const token = localStorage.getItem('token');
+
+    if(!token){
+        return <Navigate to="/login"/>
+    }
     return (
       <div className="profil">
         <EditProfil/>
