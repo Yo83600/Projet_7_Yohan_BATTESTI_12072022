687d32b1e126cfc530e293976fc5e8d1667d1afb
Yohan <yohan.battesti@gmail.com>
2022-09-01 13:30:18 +0100

    Suppression console log et ajt commentaire

--- frontend/src/components/UpdatePost/UpdatePost.jsx
+++ frontend/src/components/UpdatePost/UpdatePost.jsx
@@ -1,114 +1,120 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./UpdatePost.css";
 import axios from "axios";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen } from '@fortawesome/free-solid-svg-icons';
-const Swal = require('sweetalert2')
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
 
+const Update = ({ postId }) => {
 
-  const Update = ({postId}) => {
-  // Fonction Delete au click
   const [modal, setModal] = useState(false);
-	const [ Message,setMessage] = useState("");
-  const [image, setImage] = useState()
-    //const RefMessage = useRef();
+  const [Message, setMessage] = useState("");
+  const [image, setImage] = useState();
 
-    useEffect ( () => {
-    //Récupere tous les posts
-    axios.get(`/api/posts/${postId}`,{
-        headers:{
-					'Authorization': "Bearer " + localStorage.getItem('token')
-				}
-        })
-        .then(reponse => {
-			console.log(reponse.data)
-			//console.log(reponse.data.message)
-      //console.log(reponse.data.message)
-      // console.log(reponse.data.imageURL)
-			setMessage(reponse.data.message)
-      setImage(reponse.data.imageURL)
-        }) 
-        .catch( error => {
-              
-        })
-    },[postId])
-
-    const undleSubmit = e => {
-        e.preventDefault() // evite le rechargement
- 
-        const data = new FormData();
-        data.append('message', Message);
-        data.append('image', image);
- 
-        //Creer un post
-        axios.put(`/api/posts/${postId}`,data, {
-        body: data,
-        headers: {
-            'Authorization': "Bearer " + localStorage.getItem('token') 
-        }
-        })
-        .then(reponse => {
-          console.log(reponse.data);
-          UpdateInfo()
-        }) 
-        .catch( error => {
-        })
-    }
-
-    const UpdateInfo = () => {
-    Swal.fire({
-    icon: 'success',
-    title: 'Votre publication à été modifié',
-    showConfirmButton: false,
-    timer: 1500,
-    }).then((result) => {
-        if (result) {
-      window.location.reload()}
+  useEffect(() => {
+    // récupere les postsId
+    axios.get(`/api/posts/${postId}`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
     })
-    }
+    .then((reponse) => {
+      setMessage(reponse.data.message);
+      setImage(reponse.data.imageURL);
+    })
+    .catch((error) => {});
+  },[postId]);
 
-    const toggleModal = () => {
-      setModal(!modal);
+  const undleSubmit = (e) => {
+    e.preventDefault(); // evite le rechargement
 
-      if(modal) {
-        document.body.classList.add('active-modal')
-      } else {
-        document.body.classList.remove('active-modal')
+    // data des posts
+    const data = new FormData();
+    data.append("message", Message);
+    data.append("image", image);
+
+    // modifier un post
+    axios.put(`/api/posts/${postId}`, data, {
+      body: data,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+    .then((reponse) => {
+      console.log(reponse.data);
+      UpdateInfo();
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  };
+
+  // notification de la modification du post
+  const UpdateInfo = () => {
+    Swal.fire({
+      icon: "success",
+      title: "Votre publication à été modifié",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then((result) => {
+      if (result) {
+        window.location.reload();
       }
-    };
-	
-  return (
-    <>  <div className="update-post">
-        <FontAwesomeIcon
-          onClick={toggleModal}
-          icon={faPen}
-        />
-        </div>
+    });
+  };
 
+  // condition pour que le modal s'affiche et se ferme
+  const toggleModal = () => {
+    setModal(!modal);
+
+    if (modal) {
+      document.body.classList.add("active-modal");
+    } else {
+      document.body.classList.remove("active-modal");
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <div className="update-post">
+        <FontAwesomeIcon onClick={toggleModal} icon={faPen} />
+      </div>
       {modal && (
         <div className="modal">
-          	<div onClick={toggleModal} className="overlay"></div><div className="modal-content">
-					<h2> Modifier votre post</h2>
-					<form onSubmit={e => undleSubmit(e)}>
-						<label htmlFor="message">Message:</label>
-						<textarea
-							type="text"
-							id="message"
-              cols="30" rows="5"
-              value={Message}
-              onChange={e => setMessage(e.target.value)}
-							autoComplete="off" />
-						<input className="input-image" type="file" id="image" name="image" onChange={e => setImage(e.target.files[0])} />
-						<button id="save">Modifier</button>
-					</form>
-					<button className="close-modal" onClick={toggleModal}>
-						X
-					</button>
-				</div>
-		</div>
-	  )}
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2> Modifier votre post</h2>
+            <form onSubmit={(e) => undleSubmit(e)}>
+              <label htmlFor="message">Message:</label>
+              <textarea
+                type="text"
+                id="message"
+                cols="30"
+                rows="5"
+                value={Message}
+                onChange={(e) => setMessage(e.target.value)}
+                autoComplete="off"
+              />
+              <input
+                className="input-image"
+                type="file"
+                id="image"
+                name="image"
+                onChange={(e) => setImage(e.target.files[0])}
+              />
+              <button id="save">Modifier</button>
+            </form>
+            <button className="close-modal" onClick={toggleModal}>
+              X
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
-}
+};
 
-export default Update
+export default Update;
+
+
