46699adcdd8def3161c5d5c1715063d972f25f9d
Yohan <yohan.battesti@gmail.com>
2022-09-01 13:20:01 +0100

    Correction du fichier like et ajt commentaire

--- frontend/src/components/Like/Like.jsx
+++ frontend/src/components/Like/Like.jsx
@@ -1,76 +1,73 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-import './Like.css'
+import "./Like.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
-//import {useHistory} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
-//const POST_URL = `/api/posts/${postId.postId}/like`;
- 
-const Like = ({postId , likes, userId}) => {
-   // let history = useHistory();
-    const nbrLike = likes.length > 0 ? likes.length : 0;
-    const [nbOfLikes, setNbOfLikes] = useState(nbrLike);
-    const [postLiked, setPostLiked] = useState('');
-    //console.log(postId)
-    // console.log(postId.postId)
- 
-    const undleSubmit = e => {
-        e.preventDefault() // evite le rechargement
- 
-        const data = {
-        userLike : localStorage.getItem("user"),
-        postId : postId
-        };
-        
-        //console.log(data)
-        //console.log("Bearer"+localStorage.getItem('token'))
-        //Récupere tous les posts
-        axios.post(`/api/posts/${postId}/like`,data, {
-        headers: {
-            'Authorization': "Bearer "+localStorage.getItem('token') 
-        }
-        })
-        .then(reponse => {
-            // setNbOfLikes('')
-            // setPostLiked('')
-            const {message} = reponse.data;
-            if(message === "Post disliké"){
-              let newNbrLike = nbOfLikes - 1;
-              setNbOfLikes(newNbrLike)
-              setPostLiked(false)
-            }else{
-              let newNbrLike = nbOfLikes + 1;
-              setNbOfLikes(newNbrLike)
-              setPostLiked(true)
-            }
-           console.log(reponse.data)
-        
-           //console.log(setNbOfLikes)
-           //console.log(setPostLiked)
-        }) 
-        .catch( error => {
-        })
-    }
+const Like = ({ postId, likes }) => {
+  // variable pour verifier le nombre de likes
+  const nbrLike = likes.length > 0 ? likes.length : 0;
 
-    return (
+  // utilisation de find pour savoir le userID qui est connecté a déjà mit un like
+  const userIdCurrent = localStorage.getItem("user");
+  const userFind = likes.find((elt) => elt.userId === parseInt(userIdCurrent));
+  const postIfLike = userFind ? true : false;
+
+  const [nbOfLikes, setNbOfLikes] = useState(nbrLike);
+  const [postLiked, setPostLiked] = useState(postIfLike);
+
+  const likeSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      userLike: localStorage.getItem("user"),
+      postId: postId,
+    };
+
+    //methode post pour like le post
+    axios.post(`/api/posts/${postId}/like`, data, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+    .then((reponse) => {
+      // message du backend
+      const { message } = reponse.data;
+      // condition if/else par rapport au message renvoyé par le backend
+      if (message === "Post disliké") {
+        let newNbrLike = nbOfLikes - 1;
+        setNbOfLikes(newNbrLike);
+        setPostLiked(false);
+      } else {
+        let newNbrLike = nbOfLikes + 1;
+        setNbOfLikes(newNbrLike);
+        setPostLiked(true);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  return (
     <div className="to-interact">
       <div className="to-interact__nb-of-likes">
-          <span className={postLiked ? "button__liked" : null} onClick={undleSubmit}>
-            <FontAwesomeIcon
-              icon={faThumbsUp}
-              color={postLiked ? "#38618C" : "grey"}
-            />
-          </span>
+        <span
+          className={postLiked ? "button__liked" : null}
+          onClick={likeSubmit}
+        >
+          <FontAwesomeIcon
+            icon={faThumbsUp}
+            color={postLiked ? "#38618C" : "grey"}
+          />
+        </span>
         <p className="to-interact__nb-of-likes--number">{nbOfLikes}</p>
       </div>
-      <div className="to-interact__buttons">
-      </div>
+      <div className="to-interact__buttons"></div>
     </div>
-     )}
-  ;
-
+  );
+};
 
 export default Like;
