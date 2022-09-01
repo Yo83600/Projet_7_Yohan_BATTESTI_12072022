46711096083583cd3962d574354d8b35a010d429
Yohan <yohan.battesti@gmail.com>
2022-09-01 13:39:30 +0100

    Verification du userId pour savoir si il est admin

--- frontend/src/components/Thread/Thread.jsx
+++ frontend/src/components/Thread/Thread.jsx
@@ -1,63 +1,89 @@
-import React,{ useState,useEffect } from "react"
-import axios from '../../api/axios';
-import './Thread.css'
+import React, { useState, useEffect } from "react";
+import axios from "../../api/axios";
+import "./Thread.css";
 
 import Like from "../../components/Like/Like";
 import Delete from "../DeletePost/DeletePost";
 import Update from "../UpdatePost/UpdatePost";
-import { dateParser} from "../Utils";
+import { dateParser } from "../Utils";
 
-const POST_URL = '/api/posts/';
+const Thread = ({ token }) => {
+  const [listPost, setListPost] = useState([]);
+  const [admin, setAdmin] = useState("");
 
-const Thread = ({token}) => {
-
-    const [listPost, setListPost] = useState([]);
-    //console.log("thread" + token)
-
-    useEffect ( () => {
-    //Récupere tous les posts
-     axios.get(POST_URL,{
-        headers: {
-            'Authorization': `Bearer ${token}` 
-        }
+  //Récupere tous les posts
+  useEffect(() => {
+        axios.get("/api/posts/", {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
         })
-        .then(reponse => {
-           setListPost(reponse.data)
-           console.log(reponse.data)
-        }) 
-        .catch( error => {
-              
+        .then((reponse) => {
+            setListPost(reponse.data);
         })
-    },[token])
+        .catch((error) => {
+            console.log(error)
+        });
+    },[token]);
 
-    return (
-        <div className="App-body">
-            { listPost.map( (post, key) => {
-                return <div key={key} className="App-comment">
-                    <div className="title">
-                        <div className="photoUser">
-                        <img src={post.user.picture} className="test" alt=""/>
-                        <h2>{post.user.username}</h2></div>
-                        <div className="icon-post">
-                        {post.userId === parseInt(localStorage.getItem("user")) && <Update postId={post.id}/> }
-                        {post.userId === parseInt(localStorage.getItem("user")) && <Delete postId={post.id}/> }
-                        </div>
-                    </div>
-                    <div className="body">
-                       <p> {post.message} </p>
-                    </div>
-                    <br></br>
-                    <img src={post.imageURL} className= {post.imageURL ? "image" : null}  alt=""/>
-                    <br></br>
-                    <div className="date">
-                        <p><em>{dateParser(post.createdAt)}</em></p>
-                    </div>
-                    <Like postId={post.id} likes={post.likes}/>
-                </div>
+  // variable pour recuperer le userId dans le localstorage
+  const userID = localStorage.getItem("user");
 
-            }) }
-       </div>
-    )
-}
- 
-export default Thread
+  // récupération du userId pour savoir si c'est un admin
+  useEffect(() => {
+        //Récupere le userId
+        axios.get(`/api/auth/${userID}`, {
+            headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+        })
+        .then((reponse) => {
+            setAdmin(reponse.data.admin);
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }, [userID]);
+
+  return (
+    <div className="App-body">
+      {listPost.map((post, key) => {
+        return (
+          <div key={key} className="App-comment">
+            <div className="title">
+              <div className="photoUser">
+                <img src={post.user.picture} className="test" alt="" />
+                <h2>{post.user.username}</h2>
+              </div>
+              <div className="icon-post">
+                {post.userId === parseInt(localStorage.getItem("user")) && (
+                  <Update postId={post.id} />
+                )}
+                {(post.userId === parseInt(localStorage.getItem("user")) ||
+                  admin === true) && <Delete postId={post.id} />}
+              </div>
+            </div>
+            <div className="body">
+              <p> {post.message} </p>
+            </div>
+            <br></br>
+            <img
+              src={post.imageURL}
+              className={post.imageURL ? "image" : null}
+              alt=""
+            />
+            <br></br>
+            <div className="date">
+              <p>
+                <em>{dateParser(post.createdAt)}</em>
+              </p>
+            </div>
+            <Like postId={post.id} likes={post.likes} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Thread;
